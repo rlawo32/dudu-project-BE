9484af7a32077da70b9d2bebbe6dcb5350431305
rlawo <rlawo32@naver.com>
2024-01-24 00:57:17 +0900

    [DuDu-project] <인포> FAQ_작성_작업_20240123 (2)

---
+++ src/main/webapp/frontapp/src/information/Faq.style.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const FaqListView = styled.div`
+
+`;
+
+export const FaqWriteView = styled.div`
+
+`;
---
+++ src/main/webapp/frontapp/src/information/FaqList.tsx
@@ -0,0 +1,14 @@
+
+
+import * as Styled from "./Faq.style";
+
+const FaqList = () => {
+
+    return (
+        <Styled.FaqListView>
+
+        </Styled.FaqListView>
+    )
+}
+
+export default FaqList;

5eb8abc9c87672fc11c37363190b54afdbc7178d
rlawo <rlawo32@naver.com>
2024-01-30 06:58:00 +0900

    [DuDu-project][BE] <결제> 결제연동_작업_20240130 (12)

---
+++ src/main/java/com/cac/duduproject/jpa/domain/lecture/LectureApplication.java
@@ -0,0 +1,62 @@
+package com.cac.duduproject.jpa.domain.lecture;
+
+import com.cac.duduproject.jpa.domain.member.Member;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@NoArgsConstructor
+@Entity
+@Getter
+@Table(name = "Lecture_Application")
+public class LectureApplication {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "lecture_application_no")
+    private Long lectureApplicationNo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "lecture_no")
+    private Lecture lecture;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_no")
+    private Member member;
+
+    @Column(name = "lecture_application_cancel_yn")
+    @NotBlank
+    private String lectureApplicationCancelYn;
+
+    @Column(name = "lecture_application_cancel_desc")
+    @NotBlank
+    private String lectureApplicationCancelDesc;
+
+    @Column(name = "lecture_application_created_date")
+    @NotBlank
+    private String lectureApplicationCreatedDate;
+
+    @PrePersist
+    public void onPrePersist() {
+        this.lectureApplicationCancelYn = "N";
+        this.lectureApplicationCancelDesc = "*";
+        this.lectureApplicationCreatedDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd_HH:mm"));
+    }
+
+    public LectureApplication lectureApplicationCancel(String desc) {
+        this.lectureApplicationCancelYn = "Y";
+        this.lectureApplicationCancelDesc = desc;
+        return this;
+    }
+
+    @Builder
+    public LectureApplication(Lecture lecture, Member member) {
+        this.lecture = lecture;
+        this.member = member;
+    }
+}

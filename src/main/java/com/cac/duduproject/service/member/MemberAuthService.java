6da2a20e1fbae2d5ef06ea40462cc12ade6edb14
rlawo <rlawo32@naver.com>
2024-02-01 05:33:47 +0900

    [DuDu-project][BE] <회원> 회원로그_작업_20240201 (1)

--- src/main/java/com/cac/duduproject/service/member/MemberAuthService.java
+++ src/main/java/com/cac/duduproject/service/member/MemberAuthService.java
@@ -2,21 +2,16 @@ package com.cac.duduproject.service.member;
 
 import com.cac.duduproject.jpa.domain.member.Member;
 import com.cac.duduproject.jpa.domain.member.MemberLog;
-import com.cac.duduproject.jpa.domain.member.MemberWithdraw;
 import com.cac.duduproject.jpa.domain.member.RefreshToken;
 import com.cac.duduproject.jpa.repository.member.*;
 import com.cac.duduproject.util.EmailUtil;
-import com.cac.duduproject.util.Role;
 import com.cac.duduproject.util.jwt.JwtTokenProvider;
 import com.cac.duduproject.util.jwt.dto.JwtTokenRequestDto;
 import com.cac.duduproject.util.jwt.dto.JwtTokenResponseDto;
 import com.cac.duduproject.util.security.SecurityUtil;
 import com.cac.duduproject.web.dto.CommonResponseDto;
-import com.cac.duduproject.web.dto.member.MemberInfoResponseDto;
-import com.cac.duduproject.web.dto.member.MemberListResponseDto;
 import com.cac.duduproject.web.dto.member.MemberSignInRequestDto;
 import com.cac.duduproject.web.dto.member.MemberSignUpRequestDto;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -25,18 +20,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 @RequiredArgsConstructor
 @Service
-public class MemberService {
+public class MemberAuthService {
 
     private final MemberRepository memberRepository;
     private final MemberLogRepository memberLogRepository;
     private final MemberTermsRepository memberTermsRepository;
-    private final MemberWithdrawRepository memberWithdrawRepository;
     private final RefreshTokenRepository refreshTokenRepository;
 
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
@@ -103,6 +93,7 @@ public class MemberService {
                             .member(member)
                             .memberLogType("COMMON")
                             .memberLogSuccessYn("N")
+                            .memberLogReason("비밀번호 인증 실패")
                             .memberLogIpAddress("")
                             .build();
 
@@ -122,6 +113,7 @@ public class MemberService {
                                 .member(member)
                                 .memberLogType("COMMON")
                                 .memberLogSuccessYn("Y")
+                                .memberLogReason("JWT 인증 성공")
                                 .memberLogIpAddress("")
                                 .build();
 
@@ -180,6 +172,7 @@ public class MemberService {
         return CommonResponseDto.setSuccess("Reissue Success", tokenDto);
     }
 
+    // 권한 조회
     public String getRole() {
         try {
             Long memberNo = SecurityUtil.getCurrentMemberNo();
@@ -188,19 +181,6 @@ public class MemberService {
             return member.getRoleKey();
         } catch (Exception e) {
             return "새로고침 또는 재로그인을 해주세요.";
-        }
-    }
-
-    @Transactional
-    public CommonResponseDto<?> findMemberId(MemberSignUpRequestDto requestDto) {
-        try {
-            Member member = memberRepository.findByMemberNameAndMemberEmail(requestDto.getMemberName(), requestDto.getMemberEmail())
-                    .orElseThrow(() -> new IllegalArgumentException("해당 사용자 정보가 없습니다."));
-
-            String memberId = member.getMemberId();
-            return CommonResponseDto.setSuccess("Find MemberId Success", memberId);
-        } catch(Exception e) {
-            return CommonResponseDto.setFailed("Data Base Error!!!");
         }
     }
 
@@ -214,121 +194,6 @@ public class MemberService {
             EmailUtil.sendEntireMemberId(member.getMemberEmail(), member.getMemberId());
         } catch(Exception e) {
             e.printStackTrace();
-        }
-    }
-
-    @Transactional
-    public CommonResponseDto<?> findMemberPw(MemberSignUpRequestDto requestDto) {
-        try {
-            Member member = memberRepository.findByMemberEmail(requestDto.getMemberEmail())
-                    .orElseThrow(() -> new IllegalArgumentException("해당 사용자 정보가 없습니다."));
-
-            member.passwordUpdate(passwordEncoder.encode(requestDto.getMemberPw()));
-        } catch(Exception e) {
-            return CommonResponseDto.setFailed("Data Base Error!!!");
-        }
-        
-        return CommonResponseDto.setSuccess("Find MemberPw Success", null);
-    }
-
-    @Transactional
-    public CommonResponseDto<?> findMemberInfo() {
-        try {
-            Member member = memberRepository.findById(SecurityUtil.getCurrentMemberNo())
-                    .orElseThrow(() -> new IllegalArgumentException("해당 사용자 정보가 없습니다."));
-
-            MemberInfoResponseDto memberInfoResponseDto = new MemberInfoResponseDto(member);
-            return CommonResponseDto.setSuccess("Find MemberInfo Success", memberInfoResponseDto);
-        } catch(Exception e) {
-            return CommonResponseDto.setFailed("Data Base Error!!!");
-        }
-    }
-
-    @Transactional
-    public CommonResponseDto<?> findAllMemberList(HttpServletRequest request) {
-        List<MemberListResponseDto> list = new ArrayList<>();
-        try {
-            if(request.getParameter("role").equals("A")) {
-                list = memberRepository.findByRole(Role.ADMIN).stream()
-                        .map(MemberListResponseDto::new)
-                        .collect(Collectors.toList());
-            } else if(request.getParameter("role").equals("M")) {
-                list = memberRepository.findByRole(Role.MEMBER).stream()
-                        .map(MemberListResponseDto::new)
-                        .collect(Collectors.toList());
-            }
-        } catch(Exception e) {
-            return CommonResponseDto.setFailed("Data Base Error!!!");
-        }
-        return CommonResponseDto.setSuccess("Find MemberList Success", list);
-    }
-
-    // 회원 수정
-    @Transactional
-    public CommonResponseDto<?> memberInfoUpdate(MemberSignUpRequestDto requestDto) {
-        try {
-            Long memberNo = SecurityUtil.getCurrentMemberNo();
-            Member member = memberRepository.findById(memberNo)
-                    .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. No. : " + memberNo));
-
-            member.infoUpdate(requestDto.getMemberName(), requestDto.getMemberEmail(),
-                    requestDto.getMemberBirth(), requestDto.getMemberPhone(), requestDto.getMemberGender());
-        } catch(Exception e) {
-            return CommonResponseDto.setFailed("DataBase Error!!");
-        }
-        return CommonResponseDto.setSuccess("MemberInfo Update Success!!", null);
-    }
-
-    // 비밀번호 변경
-    @Transactional
-    public CommonResponseDto<?> passwordUpdate(HttpServletRequest request) {
-        try {
-            Member member = memberRepository.findById(SecurityUtil.getCurrentMemberNo())
-                    .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다."));
-
-            if(member.getMemberWithdrawYn().equals("Y")) {
-                return CommonResponseDto.setFailed("This Withdraw Member!!");
-            } else {
-                member.passwordUpdate(passwordEncoder.encode(request.getParameter("changePassword")));
-            }
-        } catch(Exception e) {
-            return CommonResponseDto.setFailed("DataBase Error!!");
-        }
-        return CommonResponseDto.setSuccess("Password Change Success!!", null);
-    }
-
-    // 회원 탈퇴
-    @Transactional
-    public CommonResponseDto<?> memberWithdraw(MemberSignInRequestDto requestDto) {
-        try {
-            String memberId = requestDto.getMemberId();
-            String memberPassword = requestDto.getMemberPw();
-
-            Member member = memberRepository.findByMemberId(memberId)
-                    .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. Id : " + memberId));
-            boolean matchPassword = passwordEncoder.matches(memberPassword, member.getMemberPw());
-
-            if(matchPassword) {
-                member.memberWithdrawUpdate("Y");
-
-                MemberWithdraw memberWithdraw = MemberWithdraw.builder()
-                        .memberNo(member.getMemberNo())
-                        .memberId(member.getMemberId())
-                        .memberEmail(member.getMemberEmail())
-                        .memberName(member.getMemberName())
-                        .build();
-                memberWithdrawRepository.save(memberWithdraw);
-
-                RefreshToken refreshToken = refreshTokenRepository.findByKey(String.valueOf(member.getMemberNo()))
-                        .orElseThrow(() -> new RuntimeException("해당 사용자의 토큰이 존재하지 않습니다. id : " + member.getMemberNo()));
-                refreshTokenRepository.delete(refreshToken);
-
-                return CommonResponseDto.setSuccess("Secession Success!", null);
-            } else {
-                return CommonResponseDto.setFailed("Unmatched Password!");
-            }
-        } catch(Exception e) {
-            return CommonResponseDto.setFailed("Data Base Error!");
         }
     }
 }
--- src/main/java/com/cac/duduproject/web/controller/MemberController.java
+++ src/main/java/com/cac/duduproject/web/controller/MemberController.java
@@ -1,6 +1,8 @@
 package com.cac.duduproject.web.controller;
 
-import com.cac.duduproject.service.member.MemberService;
+import com.cac.duduproject.service.member.MemberAuthService;
+import com.cac.duduproject.service.member.MemberSelectService;
+import com.cac.duduproject.service.member.MemberUpdateService;
 import com.cac.duduproject.util.EmailUtil;
 import com.cac.duduproject.util.jwt.dto.JwtTokenRequestDto;
 import com.cac.duduproject.util.jwt.dto.JwtTokenResponseDto;
@@ -19,7 +21,9 @@ import java.util.Map;
 @RequestMapping("/member")
 public class MemberController {
 
-    private final MemberService memberService;
+    private final MemberAuthService memberAuthService;
+    private final MemberUpdateService memberUpdateService;
+    private final MemberSelectService memberSelectService;
 
     @GetMapping("/sendAuthCode")
     public Map<String, Object> sendAuthCode(HttpServletRequest request) {
@@ -28,77 +32,83 @@ public class MemberController {
 
     @PostMapping("/signUp")
     public CommonResponseDto<?> signUp(@RequestBody MemberSignUpRequestDto requestDto) {
-        return memberService.signUp(requestDto);
+        return memberAuthService.signUp(requestDto);
     }
 
     @PostMapping("/signIn")
     public CommonResponseDto<JwtTokenResponseDto> signIn(@RequestBody MemberSignInRequestDto requestDto) {
-        return memberService.signIn(requestDto);
+        return memberAuthService.signIn(requestDto);
     }
 
     @GetMapping("/memberIdDuplicationChk")
     public boolean memberIdDuplicationChk(HttpServletRequest request) {
-        return memberService.memberIdDuplicationChk(request.getParameter("memberId"));
+        return memberAuthService.memberIdDuplicationChk(request.getParameter("memberId"));
     }
 
     @GetMapping("/memberEmailDuplicationChk")
     public boolean memberEmailDuplicationChk(HttpServletRequest request) {
-        return memberService.memberEmailDuplicationChk(request.getParameter("memberEmail"));
+        return memberAuthService.memberEmailDuplicationChk(request.getParameter("memberEmail"));
     }
 
     @GetMapping("/memberPwDuplicationChk")
     public boolean passwordDuplicationChk(HttpServletRequest request) {
-        return memberService.passwordDuplicationChk(request.getParameter("passwordCheck"));
+        return memberAuthService.passwordDuplicationChk(request.getParameter("passwordCheck"));
     }
 
     @PostMapping("/reissue")
     public CommonResponseDto<JwtTokenResponseDto> reissue(@RequestBody JwtTokenRequestDto requestDto, HttpServletRequest request) {
         requestDto.setAccessToken(request.getHeader("Authorization"));
-        return memberService.reissue(requestDto);
+        return memberAuthService.reissue(requestDto);
     }
 
     @GetMapping("/findMemberInfo")
     public CommonResponseDto<?> findMemberInfo() {
-        return memberService.findMemberInfo();
+        return memberSelectService.findMemberInfo();
     }
 
     @PostMapping("/findMemberId")
     public CommonResponseDto<?> findMemberId(@RequestBody MemberSignUpRequestDto requestDto) {
-        return memberService.findMemberId(requestDto);
+        return memberSelectService.findMemberId(requestDto);
+    }
+
+    @PostMapping("/findMemberPw")
+    public CommonResponseDto<?> findMemberPw(@RequestBody MemberSignUpRequestDto requestDto) {
+        return memberSelectService.findMemberPw(requestDto);
     }
 
     // 아이디 전체 이메일 전송
     @PostMapping("/entireMemberId")
     public void entireMemberId(@RequestBody MemberSignUpRequestDto requestDto) {
-        memberService.entireMemberId(requestDto);
-    }
-
-    @PostMapping("/findMemberPw")
-    public CommonResponseDto<?> findMemberPw(@RequestBody MemberSignUpRequestDto requestDto) {
-        return memberService.findMemberPw(requestDto);
+        memberAuthService.entireMemberId(requestDto);
     }
 
     // 회원 수정
     @PutMapping("/memberInfoUpdate")
     public CommonResponseDto<?> memberInfoUpdate(@RequestBody MemberSignUpRequestDto requestDto) {
-        return memberService.memberInfoUpdate(requestDto);
+        return memberUpdateService.memberInfoUpdate(requestDto);
     }
 
     // 비밀번호 확인
     @PutMapping("/passwordUpdate")
     public CommonResponseDto<?> passwordUpdate(HttpServletRequest request) {
-        return memberService.passwordUpdate(request);
+        return memberUpdateService.passwordUpdate(request);
     }
 
     // 회원 탈퇴
     @PutMapping("/memberWithdraw")
     public CommonResponseDto<?> memberWithdraw(@RequestBody MemberSignInRequestDto requestDto) {
-        return memberService.memberWithdraw(requestDto);
+        return memberUpdateService.memberWithdraw(requestDto);
+    }
+
+    // 회원 로그 조회
+    @GetMapping("/findAllMemberLog")
+    public CommonResponseDto<?> findAllMemberLog(HttpServletRequest request) {
+        return memberSelectService.findAllMemberLog(request);
     }
 
     @GetMapping("/getRole")
     public String getRole() {
-        return memberService.getRole();
+        return memberAuthService.getRole();
     }
 
     @GetMapping("/getMemberNo")

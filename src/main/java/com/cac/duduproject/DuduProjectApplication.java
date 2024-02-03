c979fe2031e4b7c48b9f8574ed43a997179f64f6
rlawo <rlawo32@naver.com>
2024-02-04 07:44:00 +0900

    [DuDu-project][BE] <오류> 타임존_작업_20240204 (1)

--- src/main/java/com/cac/duduproject/DuduProjectApplication.java
+++ src/main/java/com/cac/duduproject/DuduProjectApplication.java
@@ -1,13 +1,20 @@
 package com.cac.duduproject;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @EnableScheduling
 @SpringBootApplication
 public class DuduProjectApplication {
 
+	@PostConstruct
+	public void setTimeZone(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DuduProjectApplication.class, args);
 	}

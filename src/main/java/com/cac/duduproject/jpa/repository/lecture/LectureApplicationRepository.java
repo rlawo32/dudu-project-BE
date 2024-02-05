fe812965fa3d267d9cfeb79e20bba96daf6e055b
rlawo <rlawo32@naver.com>
2024-02-05 22:42:07 +0900

    [DuDu-project][BE] <결제> 다중결제_작업_20240204 (3)

--- src/main/java/com/cac/duduproject/jpa/repository/lecture/LectureApplicationRepository.java
+++ src/main/java/com/cac/duduproject/jpa/repository/lecture/LectureApplicationRepository.java
@@ -1,8 +1,11 @@
 package com.cac.duduproject.jpa.repository.lecture;
 
+import com.cac.duduproject.jpa.domain.lecture.Lecture;
 import com.cac.duduproject.jpa.domain.lecture.LectureApplication;
+import com.cac.duduproject.jpa.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LectureApplicationRepository extends JpaRepository<LectureApplication, Long>, LectureApplicationRepositoryCustom {
 
+    boolean existsByLectureAndMember(Lecture lecture, Member member);
 }

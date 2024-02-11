550bb71b81d1c2898f7985d81ba4209d5d4f18f0
rlawo <rlawo32@naver.com>
2024-02-11 21:11:22 +0900

    [DuDu-project][BE] <리뷰> 리뷰리스트_작업_20240211 (4)

---
+++ src/main/java/com/cac/duduproject/web/dto/community/ReviewListResponseDto.java
@@ -0,0 +1,38 @@
+package com.cac.duduproject.web.dto.community;
+
+import com.cac.duduproject.jpa.domain.community.Review;
+import lombok.Data;
+
+@Data
+public class ReviewListResponseDto {
+
+    private Long reviewNo;
+    private String reviewTitle;
+    private String reviewContent;
+    private String reviewAuthor;
+    private int reviewScore;
+    private Long institutionNo;
+    private String institutionName;
+    private String reviewCreatedDate;
+
+    private String lectureTitle;
+    private String lectureThumbnail;
+
+    public ReviewListResponseDto(Review review) {
+        this.reviewNo = review.getReviewNo();
+        this.reviewTitle = review.getReviewTitle();
+        this.reviewContent = review.getReviewContent();
+        this.reviewAuthor = review.getReviewAuthor();
+        this.reviewScore = review.getReviewScore();
+        this.institutionNo = review.getLectureInstitution().getInstitutionNo();
+        this.institutionName = review.getLectureInstitution().getInstitutionName();
+        this.reviewCreatedDate = review.getReviewCreatedDate();
+        this.lectureTitle = review.getLecture().getLectureTitle();
+        for(int i=0; i<review.getLecture().getLectureImages().size(); i++) {
+            if(review.getLecture().getLectureImages().get(i).getLectureImageType().equals("T")) {
+                this.lectureThumbnail = review.getLecture().getLectureImages().get(i).getLectureImageUrl();
+                break;
+            }
+        }
+    }
+}

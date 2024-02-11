bb1fcf9329c3ea99c6c862d2fcd22cbc39c2a88d
rlawo <rlawo32@naver.com>
2024-02-11 21:11:49 +0900

    [DuDu-project][BE] <리뷰> 리뷰리스트_작업_20240211 (7)

---
+++ src/main/java/com/cac/duduproject/web/controller/ReviewController.java
@@ -0,0 +1,34 @@
+package com.cac.duduproject.web.controller;
+
+import com.cac.duduproject.service.community.ReviewListService;
+import com.cac.duduproject.service.community.ReviewWriteService;
+import com.cac.duduproject.web.dto.CommonResponseDto;
+import com.cac.duduproject.web.dto.community.ReviewListRequestDto;
+import com.cac.duduproject.web.dto.community.ReviewWriteRequestDto;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/review")
+public class ReviewController {
+
+    private final ReviewWriteService reviewWriteService;
+    private final ReviewListService reviewListService;
+
+    @PostMapping("/reviewWrite")
+    public CommonResponseDto<?> reviewWrite(@RequestBody ReviewWriteRequestDto requestDto) {
+        return reviewWriteService.reviewWrite(requestDto);
+    }
+
+    @PostMapping("/reviewList")
+    public CommonResponseDto<?> reviewList(@RequestBody ReviewListRequestDto requestDto) {
+        return reviewListService.findAllReviewList(requestDto);
+    }
+
+    @GetMapping("/reviewDetail")
+    public CommonResponseDto<?> reviewDetail(HttpServletRequest request) {
+        return reviewListService.findReviewDetail(request);
+    }
+}

bbd1d6486bbdf619d1c5c1b62e70189cc2534015
rlawo <rlawo32@naver.com>
2024-02-13 09:46:26 +0900

    [DuDu-project][BE] <리뷰> 리뷰리스트_작업_20240213 (9)

--- src/main/java/com/cac/duduproject/service/community/ReviewWriteService.java
+++ src/main/java/com/cac/duduproject/service/community/ReviewWriteService.java
@@ -1,13 +1,17 @@
 package com.cac.duduproject.service.community;
 
+import com.cac.duduproject.jpa.domain.community.Review;
 import com.cac.duduproject.jpa.domain.lecture.Lecture;
+import com.cac.duduproject.jpa.domain.lecture.LectureApplication;
 import com.cac.duduproject.jpa.domain.member.Member;
 import com.cac.duduproject.jpa.repository.community.ReviewRepository;
+import com.cac.duduproject.jpa.repository.lecture.LectureApplicationRepository;
 import com.cac.duduproject.jpa.repository.lecture.LectureRepository;
 import com.cac.duduproject.jpa.repository.member.MemberRepository;
 import com.cac.duduproject.util.security.SecurityUtil;
 import com.cac.duduproject.web.dto.CommonResponseDto;
 import com.cac.duduproject.web.dto.community.ReviewWriteRequestDto;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,25 +22,49 @@ public class ReviewWriteService {
 
     private final ReviewRepository reviewRepository;
     private final LectureRepository lectureRepository;
+    private final LectureApplicationRepository lectureApplicationRepository;
     private final MemberRepository memberRepository;
+
+    private final ReviewImageService reviewImageService;
 
     @Transactional
     public CommonResponseDto<?> reviewWrite(ReviewWriteRequestDto requestDto) {
         try {
             Long lectureNo = requestDto.getLectureNo();
+            Long lectureApplicationNo = requestDto.getLectureApplicationNo();
             Lecture lecture = lectureRepository.findById(lectureNo)
                     .orElseThrow(() -> new IllegalArgumentException("해당 번호가 없습니다. No. : " + lectureNo));
+            LectureApplication lectureApplication = lectureApplicationRepository.findById(lectureApplicationNo)
+                    .orElseThrow(() -> new IllegalArgumentException("해당 번호가 없습니다. No. : " + lectureApplicationNo));
             Member member = memberRepository.findById(SecurityUtil.getCurrentMemberNo())
                     .orElseThrow(() -> new IllegalArgumentException("해당 번호가 없습니다. No. : " + SecurityUtil.getCurrentMemberNo()));
             requestDto.setReviewAuthor(member.getMemberName());
             requestDto.setLecture(lecture);
+            requestDto.setLectureApplication(lectureApplication);
             requestDto.setMember(member);
             requestDto.setLectureInstitution(lecture.getLectureInstitution());
 
-            reviewRepository.save(requestDto.toReview());
+            Long reviewNo = reviewRepository.save(requestDto.toReview()).getReviewNo();
+
+            reviewImageService.reviewImageInsert(reviewNo, requestDto.getReviewImage());
+            lectureApplication.lectureApplicationReview();
         } catch (Exception e) {
             return CommonResponseDto.setFailed("Data Base Error!");
         }
         return CommonResponseDto.setSuccess("Board Write Success", null);
+    }
+
+    @Transactional
+    public CommonResponseDto<?> reviewViewsUp(HttpServletRequest request) {
+        try {
+            Long reviewNo = Long.valueOf(request.getParameter("reviewNo"));
+
+            Review review = reviewRepository.findById(reviewNo)
+                    .orElseThrow(() -> new IllegalArgumentException("해당 번호가 없습니다. No. : " + reviewNo));
+            review.reviewViewsUpdate();
+        } catch (Exception e) {
+            return CommonResponseDto.setFailed("Data Base Error!");
+        }
+        return CommonResponseDto.setSuccess("Review Views Update Success", null);
     }
 }
--- src/main/java/com/cac/duduproject/web/dto/community/ReviewWriteRequestDto.java
+++ src/main/java/com/cac/duduproject/web/dto/community/ReviewWriteRequestDto.java
@@ -2,11 +2,15 @@ package com.cac.duduproject.web.dto.community;
 
 import com.cac.duduproject.jpa.domain.community.Review;
 import com.cac.duduproject.jpa.domain.lecture.Lecture;
+import com.cac.duduproject.jpa.domain.lecture.LectureApplication;
 import com.cac.duduproject.jpa.domain.lecture.LectureInstitution;
 import com.cac.duduproject.jpa.domain.member.Member;
+import com.cac.duduproject.web.dto.ImageInsertRequestDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -14,13 +18,16 @@ import lombok.NoArgsConstructor;
 public class ReviewWriteRequestDto {
 
     private Long lectureNo;
+    private Long lectureApplicationNo;
     private String reviewTitle;
     private String reviewContent;
     private String reviewAuthor;
     private int reviewScore;
     private Lecture lecture;
+    private LectureApplication lectureApplication;
     private Member member;
     private LectureInstitution lectureInstitution;
+    private List<ImageInsertRequestDto> reviewImage;
 
     public Review toReview() {
         return Review.builder()
@@ -29,6 +36,7 @@ public class ReviewWriteRequestDto {
                 .reviewAuthor(reviewAuthor)
                 .reviewScore(reviewScore)
                 .lecture(lecture)
+                .lectureApplication(lectureApplication)
                 .member(member)
                 .lectureInstitution(lectureInstitution)
                 .build();
